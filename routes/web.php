f515ee49c8f069076ff99a1a9d39f4904c682328
Sabilah Mudrikah <sabilahmudrikah2018@gmail.com>
2025-01-26 18:41:11 +0800

    feat: landing-page

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,12 @@
 
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
+Route::get('/admin', function () {
     return view('dashboard');
 });
+
+Route::get('/', function () {
+    return view('landing.pages.home');
+});
+
+
