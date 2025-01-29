02fea2e7dbaa059d8022bba776a77f9727b2ed1c
Anas Khalif <anaskhalif995@gmail.com>
2025-01-29 19:11:12 +0700

    fix : changes sidebar

--- resources/views/layouts/partials/sidebar.blade.php
+++ resources/views/layouts/partials/sidebar.blade.php
@@ -1,6 +1,6 @@
-<nav class="sidebar sidebar-offcanvas" id="sidebar">
-    <ul class="nav">
-        @role('superadmin')
+@role('admin')
+    <nav class="sidebar sidebar-offcanvas" id="sidebar">
+        <ul class="nav">
             <li class="nav-item">
                 <a class="nav-link" href="index.html">
                     <i class="icon-grid menu-icon"></i>
@@ -85,6 +85,159 @@
                     <span class="menu-title">Setting</span>
                 </a>
             </li>
-        @endrole
-    </ul>
-</nav>
+        </ul>
+    </nav>
+@endrole
+@role(['chief-editor', 'editor'])
+    <nav class="sidebar sidebar-offcanvas" id="sidebar">
+        <ul class="nav">
+            <li class="nav-item">
+                <a class="nav-link" data-toggle="collapse" href="#ui-basic" aria-expanded="false" aria-controls="ui-basic">
+                    <i class="icon-layout menu-icon"></i>
+                    <span class="menu-title">Reviewer</span>
+                    <i class="menu-arrow"></i>
+                </a>
+                <div class="collapse" id="ui-basic">
+                    <ul class="nav flex-column sub-menu">
+                        <li class="nav-item"> <a class="nav-link" href="pages/ui-features/buttons.html">Need to be
+                                reviewed</a></li>
+                        <li class="nav-item"> <a class="nav-link" href="pages/ui-features/dropdowns.html">Review
+                                completed</a></li>
+                        <li class="nav-item"> <a class="nav-link" href="pages/ui-features/typography.html">Closed by
+                                editor</a></li>
+                    </ul>
+                </div>
+            </li>
+
+            <li class="nav-item">
+                <a class="nav-link" data-toggle="collapse" href="#form-elements" aria-expanded="false"
+                    aria-controls="form-elements">
+                    <i class="icon-columns menu-icon"></i>
+                    <span class="menu-title">Reviewer: Paper</span>
+                    <i class="menu-arrow"></i>
+                </a>
+                <div class="collapse" id="form-elements">
+                    <ul class="nav flex-column sub-menu">
+                        <li class="nav-item"> <a class="nav-link" href="pages/ui-features/buttons.html">Need to be
+                                reviewed</a></li>
+                        <li class="nav-item"> <a class="nav-link" href="pages/ui-features/dropdowns.html">Review
+                                completed</a></li>
+                        <li class="nav-item"> <a class="nav-link" href="pages/ui-features/typography.html">Closed by
+                                editor</a></li>
+                    </ul>
+                </div>
+            </li>
+
+            <li class="nav-item">
+                <a class="nav-link" data-toggle="collapse" href="#charts" aria-expanded="false" aria-controls="charts">
+                    <i class="icon-bar-graph menu-icon"></i>
+                    <span class="menu-title">Editor</span>
+                    <i class="menu-arrow"></i>
+                </a>
+                <div class="collapse" id="charts">
+                    <ul class="nav flex-column sub-menu">
+                        <li class="nav-item"> <a class="nav-link" href="pages/ui-features/buttons.html">No assignment</a>
+                        </li>
+                        <li class="nav-item"> <a class="nav-link" href="pages/ui-features/dropdowns.html">No review</a>
+                        </li>
+                        <li class="nav-item"> <a class="nav-link" href="pages/ui-features/typography.html">No
+                                decision</a></li>
+                        <li class="nav-item"> <a class="nav-link" href="pages/ui-features/typography.html">With
+                                decision</a></li>
+                        <li class="nav-item"> <a class="nav-link" href="pages/ui-features/typography.html">All
+                                abstracts</a></li>
+                        <li class="nav-item"> <a class="nav-link" href="pages/ui-features/typography.html">Reviewers
+                                Workload</a></li>
+                    </ul>
+                </div>
+            </li>
+
+            <li class="nav-item">
+                <a class="nav-link" data-toggle="collapse" href="#tables" aria-expanded="false" aria-controls="tables">
+                    <i class="icon-grid-2 menu-icon"></i>
+                    <span class="menu-title">Editor: Paper</span>
+                    <i class="menu-arrow"></i>
+                </a>
+                <div class="collapse" id="tables">
+                    <ul class="nav flex-column sub-menu">
+                        <li class="nav-item"> <a class="nav-link" href="pages/ui-features/buttons.html">No assignment</a>
+                        </li>
+                        <li class="nav-item"> <a class="nav-link" href="pages/ui-features/dropdowns.html">Under
+                                review</a></li>
+                        <li class="nav-item"> <a class="nav-link" href="pages/ui-features/typography.html">Revision
+                                req</a></li>
+                        <li class="nav-item"> <a class="nav-link" href="pages/ui-features/typography.html">With
+                                decision</a></li>
+                        <li class="nav-item"> <a class="nav-link" href="pages/ui-features/typography.html">All papers</a>
+                        </li>
+                        <li class="nav-item"> <a class="nav-link" href="pages/ui-features/typography.html">Reviewers
+                                Workload</a></li>
+                    </ul>
+                </div>
+            </li>
+
+        </ul>
+    </nav>
+@endrole
+
+@role('reviewer')
+    <nav class="sidebar sidebar-offcanvas" id="sidebar">
+        <ul class="nav">
+            <li class="nav-item">
+                <a class="nav-link" data-toggle="collapse" href="#ui-basic" aria-expanded="false"
+                    aria-controls="ui-basic">
+                    <i class="icon-layout menu-icon"></i>
+                    <span class="menu-title">Reviewer</span>
+                    <i class="menu-arrow"></i>
+                </a>
+                <div class="collapse" id="ui-basic">
+                    <ul class="nav flex-column sub-menu">
+                        <li class="nav-item"> <a class="nav-link" href="pages/ui-features/buttons.html">Need to be
+                                reviewed</a></li>
+                        <li class="nav-item"> <a class="nav-link" href="pages/ui-features/dropdowns.html">Review
+                                completed</a></li>
+                    </ul>
+                </div>
+            </li>
+
+            <li class="nav-item">
+                <a class="nav-link" data-toggle="collapse" href="#form-elements" aria-expanded="false"
+                    aria-controls="form-elements">
+                    <i class="icon-columns menu-icon"></i>
+                    <span class="menu-title">Reviewer: Paper</span>
+                    <i class="menu-arrow"></i>
+                </a>
+                <div class="collapse" id="form-elements">
+                    <ul class="nav flex-column sub-menu">
+                        <li class="nav-item"> <a class="nav-link" href="pages/ui-features/buttons.html">Need to be
+                                reviewed</a></li>
+                        <li class="nav-item"> <a class="nav-link" href="pages/ui-features/dropdowns.html">Review
+                                completed</a></li>
+                    </ul>
+                </div>
+            </li>
+
+        </ul>
+    </nav>
+@endrole
+
+@role(['indonesia-presenter', 'foreign-presenter'], ['indonesia-participants', 'foreign-participants'])
+    <nav class="sidebar sidebar-offcanvas" id="sidebar">
+        <ul class="nav">
+            <li class="nav-item">
+                <a class="nav-link" data-toggle="collapse" href="#ui-basic" aria-expanded="false"
+                    aria-controls="ui-basic">
+                    <i class="icon-layout menu-icon"></i>
+                    <span class="menu-title">Abstract</span>
+                </a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link" data-toggle="collapse" href="#ui-basic" aria-expanded="false"
+                    aria-controls="ui-basic">
+                    <i class="icon-layout menu-icon"></i>
+                    <span class="menu-title">Payment</span>
+                </a>
+            </li>
+        </ul>
+    </nav>
+@endrole
--- routes/web.php
+++ routes/web.php
@@ -4,19 +4,23 @@ use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\Admin\UserController;
 use App\Http\Controllers\Admin\ViewerController;
+use App\Http\Controllers\Admin\SymposiumController;
+use App\Http\Controllers\AbstractController;
 
 Route::get('/', function () {
     return "Welcome to Laravel 11";
 });
 
 Route::name('admin.')->prefix('admin')->namespace('App\Http\Controllers\Admin')->middleware(['auth', 'role:admin'])->group(function () {
-    Route::resource('participants', 'UserController');
-    Route::resource('reviewers', 'ViewerController');
+    Route::resource('participant', 'UserController');
+    Route::resource('reviewer', 'ViewerController');
+    Route::resource('symposium', 'SymposiumController');
 });
 
 Route::get('/dashboard', function () {
     return view('dashboard');
 })->middleware(['auth'])->name('dashboard');
+
 
 Route::get('/user', function () {
     return request()->user();
@@ -26,6 +30,10 @@ Route::middleware('auth')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+});
+
+Route::middleware('auth')->group(function () {
+    Route::resource('abstracts', AbstractController::class);
 });
 
 require __DIR__ . '/auth.php';
