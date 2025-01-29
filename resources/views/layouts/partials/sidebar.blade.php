2181d4186d1960b094bf105d816d1f58ddb9787d
Anas Khalif <anaskhalif995@gmail.com>
2025-01-29 16:54:47 +0700

    fix : change sidebar

--- resources/views/layouts/partials/sidebar.blade.php
+++ resources/views/layouts/partials/sidebar.blade.php
@@ -1,108 +1,90 @@
-<!-- partial:partials/_sidebar.html -->
 <nav class="sidebar sidebar-offcanvas" id="sidebar">
     <ul class="nav">
-        <li class="nav-item">
-            <a class="nav-link" href="index.html">
-                <i class="icon-grid menu-icon"></i>
-                <span class="menu-title">Dashboard</span>
-            </a>
-        </li>
-        <li class="nav-item">
-            <a class="nav-link" data-toggle="collapse" href="#ui-basic" aria-expanded="false" aria-controls="ui-basic">
-                <i class="icon-layout menu-icon"></i>
-                <span class="menu-title">UI Elements</span>
-                <i class="menu-arrow"></i>
-            </a>
-            <div class="collapse" id="ui-basic">
-                <ul class="nav flex-column sub-menu">
-                    <li class="nav-item"> <a class="nav-link" href="pages/ui-features/buttons.html">Buttons</a></li>
-                    <li class="nav-item"> <a class="nav-link" href="pages/ui-features/dropdowns.html">Dropdowns</a></li>
-                    <li class="nav-item"> <a class="nav-link" href="pages/ui-features/typography.html">Typography</a>
-                    </li>
-                </ul>
-            </div>
-        </li>
-        <li class="nav-item">
-            <a class="nav-link" data-toggle="collapse" href="#form-elements" aria-expanded="false"
-                aria-controls="form-elements">
-                <i class="icon-columns menu-icon"></i>
-                <span class="menu-title">Form elements</span>
-                <i class="menu-arrow"></i>
-            </a>
-            <div class="collapse" id="form-elements">
-                <ul class="nav flex-column sub-menu">
-                    <li class="nav-item"><a class="nav-link" href="pages/forms/basic_elements.html">Basic Elements</a>
-                    </li>
-                </ul>
-            </div>
-        </li>
-        <li class="nav-item">
-            <a class="nav-link" data-toggle="collapse" href="#charts" aria-expanded="false" aria-controls="charts">
-                <i class="icon-bar-graph menu-icon"></i>
-                <span class="menu-title">Charts</span>
-                <i class="menu-arrow"></i>
-            </a>
-            <div class="collapse" id="charts">
-                <ul class="nav flex-column sub-menu">
-                    <li class="nav-item"> <a class="nav-link" href="pages/charts/chartjs.html">ChartJs</a></li>
-                </ul>
-            </div>
-        </li>
-        <li class="nav-item">
-            <a class="nav-link" data-toggle="collapse" href="#tables" aria-expanded="false" aria-controls="tables">
-                <i class="icon-grid-2 menu-icon"></i>
-                <span class="menu-title">Tables</span>
-                <i class="menu-arrow"></i>
-            </a>
-            <div class="collapse" id="tables">
-                <ul class="nav flex-column sub-menu">
-                    <li class="nav-item"> <a class="nav-link" href="pages/tables/basic-table.html">Basic table</a></li>
-                </ul>
-            </div>
-        </li>
-        <li class="nav-item">
-            <a class="nav-link" data-toggle="collapse" href="#icons" aria-expanded="false" aria-controls="icons">
-                <i class="icon-contract menu-icon"></i>
-                <span class="menu-title">Icons</span>
-                <i class="menu-arrow"></i>
-            </a>
-            <div class="collapse" id="icons">
-                <ul class="nav flex-column sub-menu">
-                    <li class="nav-item"> <a class="nav-link" href="pages/icons/mdi.html">Mdi icons</a></li>
-                </ul>
-            </div>
-        </li>
-        <li class="nav-item">
-            <a class="nav-link" data-toggle="collapse" href="#auth" aria-expanded="false" aria-controls="auth">
-                <i class="icon-head menu-icon"></i>
-                <span class="menu-title">User Pages</span>
-                <i class="menu-arrow"></i>
-            </a>
-            <div class="collapse" id="auth">
-                <ul class="nav flex-column sub-menu">
-                    <li class="nav-item"> <a class="nav-link" href="pages/samples/login.html"> Login </a></li>
-                    <li class="nav-item"> <a class="nav-link" href="pages/samples/register.html"> Register </a></li>
-                </ul>
-            </div>
-        </li>
-        <li class="nav-item">
-            <a class="nav-link" data-toggle="collapse" href="#error" aria-expanded="false" aria-controls="error">
-                <i class="icon-ban menu-icon"></i>
-                <span class="menu-title">Error pages</span>
-                <i class="menu-arrow"></i>
-            </a>
-            <div class="collapse" id="error">
-                <ul class="nav flex-column sub-menu">
-                    <li class="nav-item"> <a class="nav-link" href="pages/samples/error-404.html"> 404 </a></li>
-                    <li class="nav-item"> <a class="nav-link" href="pages/samples/error-500.html"> 500 </a></li>
-                </ul>
-            </div>
-        </li>
-        <li class="nav-item">
-            <a class="nav-link" href="pages/documentation/documentation.html">
-                <i class="icon-paper menu-icon"></i>
-                <span class="menu-title">Documentation</span>
-            </a>
-        </li>
+        @role('superadmin')
+            <li class="nav-item">
+                <a class="nav-link" href="index.html">
+                    <i class="icon-grid menu-icon"></i>
+                    <span class="menu-title">Summary</span>
+                </a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link" href="participants.html">
+                    <i class="mdi mdi-account-star menu-icon"></i>
+                    <span class="menu-title">Participants</span>
+                </a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link" href="abstracts.html">
+                    <i class="mdi mdi-book-open-page-variant menu-icon"></i>
+                    <span class="menu-title">Abstracts</span>
+                </a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link" href="oral-distribution.html">
+                    <i class="icon-grid menu-icon"></i>
+                    <span class="menu-title">Oral Distribution</span>
+                </a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link" href="symposiums.html">
+                    <i class="icon-grid menu-icon"></i>
+                    <span class="menu-title">Symposiums</span>
+                </a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link" href="payment.html">
+                    <i class="icon-grid menu-icon"></i>
+                    <span class="menu-title">Payment</span>
+                </a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link" href="manual-receipt.html">
+                    <i class="icon-grid menu-icon"></i>
+                    <span class="menu-title">Manual Receipt</span>
+                </a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link" href="certificates.html">
+                    <i class="icon-grid menu-icon"></i>
+                    <span class="menu-title">Certificates</span>
+                </a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link" href="reviewers.html">
+                    <i class="icon-grid menu-icon"></i>
+                    <span class="menu-title">Reviewers</span>
+                </a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link" href="email-csv.html">
+                    <i class="icon-grid menu-icon"></i>
+                    <span class="menu-title">Email CSV</span>
+                </a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link" href="abstracts-book.html">
+                    <i class="icon-grid menu-icon"></i>
+                    <span class="menu-title">Abstracts Book</span>
+                </a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link" href="download-files.html">
+                    <i class="icon-grid menu-icon"></i>
+                    <span class="menu-title">Download Files</span>
+                </a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link" href="upload.html">
+                    <i class="icon-grid menu-icon"></i>
+                    <span class="menu-title">Upload</span>
+                </a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link" href="setting.html">
+                    <i class="icon-grid menu-icon"></i>
+                    <span class="menu-title">Setting</span>
+                </a>
+            </li>
+        @endrole
     </ul>
 </nav>
