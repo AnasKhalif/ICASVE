37889cfc2fdb0d0badffbb4acb6771c3e9f539f0
Ahmad Surya Alam Syah <alamsyah200527@gmail.com>
2025-02-13 22:14:39 +0700

    update 3

--- resources/views/landingpage/home.blade.php
+++ resources/views/landingpage/home.blade.php
@@ -44,9 +44,9 @@
 
 <!-- About Section -->
 <section id="about" class="about section">
-    <div class="container" data-aos="fade-up" data-aos-delay="100">
+    <div class="container" >
         <div class="row align-items-center justify-content-between gy-5">
-            <div class="col-12 col-xl-5 content">
+            <div class="teks-about col-12 col-xl-5 content"  data-aos="fade-up">
                 <h3>About</h3>
                 <h2>ICASVE 2024</h2>
                 <p>
@@ -62,8 +62,8 @@
         
             <div class="d-none d-xl-block col-xl-2"></div>
         
-            <div class="col-12 col-xl-5 d-flex justify-content-end">
-                <img src="../images/Lab-Vokasi.jpg" class="w-100 w-md-75 animated rounded-3" alt="gedung-vokasi" />
+            <div class="image-about col-12 col-xl-5 d-flex justify-content-end"  data-aos="fade-up" data-aos-delay="100">
+                <img src="{{asset('/images/Lab-Vokasi.jpg')}}" class="w-100 w-md-75 animated rounded-3" alt="gedung-vokasi" />
             </div>
         </div>
     </div>
@@ -226,9 +226,9 @@
 <div id="hero-topics" class="py-5 content-topics">
     <div class="container">
         <div class="contents-topics text-white text-left text-md-start">
-            <h2 class="fw-bold text-white mb-3">CALL FOR PAPER</h2>
-            <p>The topics include, but are not limited to:</p>
-            <ul class="list-topics">
+            <h2 class="fw-bold text-white mb-3" data-aos="fade-up">CALL FOR PAPER</h2>
+            <p data-aos="fade-up">The topics include, but are not limited to:</p>
+            <ul class="list-topics" data-aos="fade-up" data-aos-delay="200">
                 <li>Economic and Business</li>
                 <li>Technological Engineering</li>
                 <li>Design Innovation</li>
@@ -245,23 +245,20 @@
 <!-- Details Section -->
 <section id="details" class="details section">
    <div class="container"> 
-    <!-- Section Title -->
-    <!-- End Section Title -->
         <!-- Features Item -->
-
         <div class="row gy-4 align-items-center features-item">
             <div class=" col-md-5 order-1 order-md-2 d-flex align-items-center" data-aos="zoom-out" data-aos-delay="200">
                 <img src="{{asset('images/poster.png')}}" class="poster-md img-fluid rounded-2" alt="" />
             </div>
-            <div class="col-md-7 order-2 order-md-1" data-aos="fade-up" data-aos-delay="200">
+            <div class="col-md-7 order-2 order-md-1">
                 <div class="container section-title" data-aos="fade-up">
                     <h2>ICASVE Conference Timeline</h2>
                     <div><span>Deadline</span> <span class="description-title"> Dates</span></div>
                 </div>
-                <div class="mb-4">
+                <div class="mb-4" data-aos="fade-up" data-aos-delay="100">
                     <img src="{{asset('images/poster.png')}}" class="poster-sm img-fluid rounded-2" alt="" />
                 </div>
-                <table class="custom-table">
+                <table class="custom-table" data-aos="fade-up" data-aos-delay="200">
                     <thead>
                         <tr>
                             <th>Name</th>
@@ -299,7 +296,7 @@
                         </tr>
                     </tbody>
                 </table>
-                <a href="" class="leaflet-button">Get Our Leaflet</a>
+                <a href="" class="leaflet-button" data-aos="fade-up" >Get Our Leaflet</a>
             </div>
         </div>
         <!-- Features Item -->
@@ -310,7 +307,7 @@
                     <h2>Registration</h2>
                     <div><span>Registration</span> <span class="description-title"> Fee</span></div>
                 </div>
-                <div class="table-responsive">
+                <div class="table-responsive" data-aos="fade-up" data-aos-delay="100">
                     <table class="table table-bordered">
                         <thead class="thead-light">
                             <tr>
@@ -361,7 +358,7 @@
                         </tbody>
                     </table>
 
-                    <table class="table table-bordered">
+                    <table class="table table-bordered" data-aos="fade-up">
                         <thead class="thead-light">
                             <tr>
                                 <th>Additional Fee</th>
@@ -381,7 +378,7 @@
                     </table>
                 </div>
 
-                <p class="text-muted mt-3">
+                <p class="text-muted mt-3" data-aos="fade-up">
                     <small>
                         <b>Please note:</b><br />
                         1. The registration fee serves only for conference fee, and it cannot be waived for authors.<br />
@@ -391,7 +388,7 @@
                 </p>
 
                 <div class="d-flex justify-content-between align-items-center row text-center mt-5">
-                    <div class="bank col-md-4">
+                    <div class="bank col-md-4" data-aos="fade-up" data-aos-delay="100">
                         <div class="card border-primary">
                             <div class="card-body">
                                 <p><b>Bank Mandiri</b></p>
@@ -401,7 +398,7 @@
                             </div>
                         </div>
                     </div>
-                    <div class="bank col-md-4">
+                    <div class="bank col-md-4" data-aos="fade-up" data-aos-delay="300">
                         <div class="card border-warning">
                             <div class="card-body">
                                 <p><b>Bank Nasional Indonesia</b></p>
@@ -411,7 +408,7 @@
                             </div>
                         </div>
                     </div>
-                    <div class="bank col-md-4">
+                    <div class="bank col-md-4" data-aos="fade-up" data-aos-delay="500">
                         <div class="card border-danger">
                             <div class="card-body">
                                 <p><b>Bank Jatim</b></p>
@@ -423,7 +420,7 @@
                     </div>
                 </div>
 
-                <div class="text-start mt-4">
+                <div class="text-start mt-4" data-aos="fade-up">
                     <button class="btn btn-success btn-sm btn-confirm">Payment Proof Upload and Attendance Confirmation -></button>
                 </div>
             </div>
@@ -436,21 +433,21 @@
                 <div><span>Our</span> <span class="description-title"> Venue</span></div>
             </div>
             <div class="row align-items-start">
-                <div class="col-md-6">
+                <div class="col-md-6" data-aos="fade-up">
                     <img
                         src="{{asset('images/widyaloka.png')}}"
                         alt="Venue Building"
                         class="img-fluid rounded shadow"
                         style="width: 100%; max-width: 500px; height: auto" />
                 </div>
-                <div class="location col-md-6">
+                <div class="location col-md-6" data-aos="fade-up" >
                     <h3 class="title-location">Convention Hall Universitas Brawijaya</h3>
                     <p class="text-muted">Jl. Seminar Raya No. 1, Kota Event, Indonesia</p>
                     <p class="font-weight-bold">Date: November 20th - 22nd, 2024</p>
                     <a href="https://maps.app.goo.gl/5goJdTDuFaAWxUWG9" target="_blank" class="btn btn-primary btn-map">See on Map</a>
                 </div>
             </div>
-            <div class="mt-4">
+            <div class="mt-4" data-aos="fade-up" >
                 <iframe
                     src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3951.4566125772426!2d112.6110645750068!3d-7.951675092072795!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x2e78827f2d620cf1%3A0xf45788aac2a0e437!2sConvention%20Hall%20Universitas%20Brawijaya!5e0!3m2!1sid!2sid!4v1737982475129!5m2!1sid!2sid"
                     width="100%"
@@ -474,22 +471,22 @@
             <div><span>Publications</span> <span class="description-title"> Journal</span></div>
         </div>
         <div class="row text-center" style="display: flex; align-items: center">
-            <div class="col-md-2 col-6 mb-3">
+            <div class="col-md-2 col-6 mb-3" data-aos="fade-up">
                 <img src="{{asset('images/logo/logo-1.png')}}" alt="Logo 1" class="img-fluid" />
             </div>
-            <div class="col-md-2 col-6 mb-3">
+            <div class="col-md-2 col-6 mb-3" data-aos="fade-up">
                 <img src="{{asset('images/logo/logo-2.png')}}" alt="Logo 2" class="img-fluid" />
             </div>
-            <div class="col-md-2 col-6 mb-3">
+            <div class="col-md-2 col-6 mb-3" data-aos="fade-up">
                 <img src="{{asset('images/logo/logo-3.jpg')}}" alt="Logo 3" class="img-fluid" />
             </div>
-            <div class="col-md-2 col-6 mb-3">
+            <div class="col-md-2 col-6 mb-3" data-aos="fade-up">
                 <img src="{{asset('images/logo/logo-4.png')}}" alt="Logo 4" class="img-fluid" />
             </div>
-            <div class="col-md-2 col-6 mb-3">
+            <div class="col-md-2 col-6 mb-3" data-aos="fade-up">
                 <img src="{{asset('images/logo/logo-5.png')}}" alt="Logo 5" class="img-fluid" />
             </div>
-            <div class="col-md-2 col-6 mb-3">
+            <div class="col-md-2 col-6 mb-3" data-aos="fade-up">
                 <img src="{{asset('images/logo/logo-6.jpeg')}}" alt="Logo 6" class="img-fluid" />
             </div>
         </div>
@@ -503,52 +500,52 @@
             <!-- Left Section (30%) -->
             <div class="col-md-4 text-white p-4" style="background-color: #fbfbfb;">
                 <div class="mb-4 text-center">
-                    <h4>Hosted By</h4>
-                    <img src="{{asset('images/logo/logo-hostedBy.png')}}" alt="Hosted Logo" class="img-fluid mb-2 w-50 mx-auto">
+                    <h4 data-aos="fade-up">Hosted By</h4>
+                    <img src="{{asset('images/logo/logo-hostedBy.png')}}" alt="Hosted Logo" class="img-fluid mb-2 w-50 mx-auto" data-aos="fade-up">
                 </div>
                 <div class="text-center">
-                    <h4>Co-Hosted By</h4>
+                    <h4 data-aos="fade-up">Co-Hosted By</h4>
                     <img src="{{asset('images/logo/logo-co1.png')}}" class="w-50 mx-auto mb-2" alt="Co-Hosted Logo 1"
-                        class="img-fluid mb-2">
+                        class="img-fluid mb-2" data-aos="fade-up">
                     <img src="{{asset('images/logo/logo-co2.png')}}" class="w-50 mx-auto" alt="Co-Hosted Logo 2"
-                        class="img-fluid mb-2">
+                        class="img-fluid mb-2" data-aos="fade-up">
                 </div>
             </div>
             <!-- Right Section (70%) -->
             <div class="col-md-8 p-4">
-                <h4 class="text-center mb-4">Supported By</h4>
+                <h4 class="text-center mb-4" data-aos="fade-up">Supported By</h4>
                 <div class="row text-center">
                     <div class="col-md-3 col-6 mb-3">
                         <img src="{{asset('images/logo/logo-7.png')}}" class="logo-support" alt="Sponsor Logo 1"
-                            class="img-fluid">
+                            class="img-fluid" data-aos="fade-up">
                     </div>
                     <div class="col-md-3 col-6 mb-3">
                         <img src="{{asset('images/logo/logo-8.png')}}" class="logo-support" alt="Sponsor Logo 2"
-                            class="img-fluid">
+                            class="img-fluid" data-aos="fade-up">
                     </div>
                     <div class="col-md-3 col-6 mb-3">
                         <img src="{{asset('images/logo/logo-9.jpg')}}" class="logo-support" alt="Sponsor Logo 3"
-                            class="img-fluid">
+                            class="img-fluid" data-aos="fade-up">
                     </div>
                     <div class="col-md-3 col-6 mb-3">
                         <img src="{{asset('images/logo/logo-10.png')}}" class="logo-support" alt="Sponsor Logo 4"
-                            class="img-fluid">
+                            class="img-fluid" data-aos="fade-up">
                     </div>
                     <div class="col-md-3 col-6 mb-3">
                         <img src="{{asset('images/logo/logo-11.jpg')}}" class="logo-support" alt="Sponsor Logo 4"
-                            class="img-fluid">
+                            class="img-fluid" data-aos="fade-up">
                     </div>
                     <div class="col-md-3 col-6 mb-3">
                         <img src="{{asset('images/logo/logo-12.png')}}" class="logo-support" alt="Sponsor Logo 4"
-                            class="img-fluid">
+                            class="img-fluid" data-aos="fade-up">
                     </div>
                     <div class="col-md-3 col-6 mb-3">
                         <img src="{{asset('images/logo/logo-13.jpeg')}}" class="logo-support" alt="Sponsor Logo 4"
-                            class="img-fluid">
+                            class="img-fluid" data-aos="fade-up">
                     </div>
                     <div class="col-md-3 col-6 mb-3">
                         <img src="{{asset('images/logo/logo-14.png')}}" class="logo-support" alt="Sponsor Logo 4"
-                            class="img-fluid">
+                            class="img-fluid" data-aos="fade-up">
                     </div>
                 </div>
             </div>
