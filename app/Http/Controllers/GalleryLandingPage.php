0b2ef21f4d714e487ee17567f9c83f91249b13bf
Raka Sanjaya <sanjayaraka722@gmail.com>
2025-02-20 17:47:57 +0700

    feat/revision 20 feb

--- app/Http/Controllers/GalleryLandingPage.php
+++ app/Http/Controllers/GalleryLandingPage.php
@@ -3,14 +3,14 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
-use App\Models\gallery;
+use App\Models\Gallery;
 
 class GalleryLandingPage extends Controller
 {
     public function index()
     {
-        $gallery = gallery::all();
-        $years = gallery::select('year')->distinct()->orderBy('year', 'desc')->get();
+        $gallery = Gallery::all();
+        $years = Gallery::select('year')->distinct()->orderBy('year', 'desc')->get();
         return view('landingpage.gallery.gallery', compact('gallery', 'years'));
     }
 }
