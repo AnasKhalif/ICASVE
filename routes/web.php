0fc469b245b9fbd4cf598c20163688d1151bc43a
Anas Khalif <anaskhalif995@gmail.com>
2025-03-02 11:59:19 +0700

    feat : add profile

--- routes/web.php
+++ routes/web.php
@@ -242,6 +242,7 @@ Route::middleware('auth')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+    Route::post('/profile/upload-photo', [ProfileController::class, 'uploadPhoto'])->name('profile.uploadPhoto');
 });
 
 require __DIR__ . '/auth.php';
