7106b57f378ba39a85df6e48197e05006e6e6dce
Ahmad Surya Alam Syah <alamsyah200527@gmail.com>
2025-03-18 15:53:42 +0700

    fix

--- resources/views/landingpage-editor/conference/conference-detail/edit.blade.php
+++ resources/views/landingpage-editor/conference/conference-detail/edit.blade.php
@@ -75,32 +75,6 @@
 
                 </form>
             </div>
-            <div class="mb-4">
-                <label class="form-label">Theme</label>
-                <input type="text" class="form-control" name="theme" value="{{ $conferenceDetail->theme }}" required>
-                <small>Example: International Conference on Applied Science for Vocational Education</small>
-            </div>
-            <div class="mb-4">
-                <label class="form-label">University</label>
-                <input type="text" class="form-control" name="university" value="{{ $conferenceDetail->university }}" required>
-                <small>Example: Hybrid Conference | Faculty of Vocational Studies, Universitas Brawijaya</small>
-            </div>
-            <div class="mb-4">
-                <label class="form-label">Hosted By or Co-Hosted By</label>
-                <input type="text" class="form-control" name="hosted" value="{{ $conferenceDetail->hosted }}" required>
-                <small>Example: Co-hosted by State Polytechnic of Batam & State University of Malang</small>
-            </div>
-            <div class="mb-4">
-                <label class="form-label">Date</label>
-                <input type="date" class="form-control" name="date" value="{{ $conferenceDetail->date }}" required>
-                <small>Example: 4th International Conference on Applied Science for Vocational Education (ICASVE) 2025, Batam, Indonesia</small>
-            </div>
-            <div>
-                <label class="form-label">Year</label>
-                <input type="text" class="form-control" name="year" value="{{ $conferenceDetail->year }}" required>
-                <small>Example: 2025</small>
-            </div>
-            <button type="submit" class="btn btn-success">Save</button>
-        </form>
+        </div>
     </div>
 @endsection
